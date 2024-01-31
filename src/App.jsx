2f851a6caaf517be5046fa07dcf0aef9f5975dc1
Op-Pautu <pautuop@gmail.com>
2024-01-31 21:09:20 +0530

    Fetch data from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,40 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://api.tvmaze.com/search/shows?q=all"
+        );
+        const data = await response.json();
+        setItems(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    console.log(items);
+  }, [items]);
+
   return (
     <div className="h-full flex items-center justify-center">
-      <div className="text-red-800 text-lg flex items-center ">
-        Installing vite and tailwindcss
-      </div>
+      <h1 className="text-5xl text-sky-600 flex items-center">QuadB Tech</h1>
+
+      <main>
+        <div>
+          {items?.map((item) => (
+            <div key={item.show.id}>{item.show.name}</div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
---
+++ src/components/Card.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="w-[350px] rounded-md p-4">
+      <h1>title</h1>
+      <p>test</p>
+    </div>
+  );
+};
+
+export default Card;
