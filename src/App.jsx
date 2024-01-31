5c5021ec81cd9ee3fcdbfc1cacf48cdf31d99274
Op-Pautu <pautuop@gmail.com>
2024-01-31 17:27:40 +0530

    Add initial files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="h-full flex items-center justify-center">
+      <div className="text-red-800 text-lg flex items-center ">
+        Installing vite and tailwindcss
+      </div>
+    </div>
+  );
+}
+
+export default App;
